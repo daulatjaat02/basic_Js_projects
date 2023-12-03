4370c1a3b7712d97858eebb74b6b132d594ebd1b
daulat <daulatjajra2006@gmail.com>
2023-12-03 14:45:53 +0530

    'final'

---
+++ modals/script.js
@@ -0,0 +1,19 @@
+let modal = document.querySelector(".modal");
+let overlay = document.querySelector(".overlay");
+let btnCloseModal = document.querySelector(".close");
+let btnsShowModal = document.querySelectorAll(".btn");
+
+let openModal = () => {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+let closeModal = () => {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+for (let i = 0; i < btnsShowModal.length; i++) {
+  btnsShowModal[i].addEventListener("click", openModal);
+}
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
