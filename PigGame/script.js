49b1d545815164354a0d7fb81e218e804240e24d
Daulat Jajra <you@example.com>
2023-12-08 11:31:46 +0530

    set

---
+++ PigGame/script.js
@@ -0,0 +1,19 @@
+"use strict ";
+
+// let score0El = document.getElementById("score--0");
+// let score1El = document.querySelector("#score--1");
+// let diceEl = document.querySelector(".dice ");
+// let btnNew = document.querySelector(".btn--new");
+// let btnRoll = document.querySelector(".btn--roll");
+// let btnHold = document.querySelector(".btn--hold");
+
+// score0El.textContent = 0;
+// score1El.textContent = 0;
+// diceEl.classList.add("hidden");
+
+// btnRoll.addEventListener("click", () => {
+//   let dice = Math.floor(Math.random() * 6) + 1;
+//   console.log(dice);
+//   diceEl.classList.remove("hidden");
+//   diceEl.src = `Imgs/${dice}.png`;
+// });
